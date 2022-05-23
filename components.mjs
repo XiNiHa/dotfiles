053b9c06b727f9e6a8f3f5812c64c656fd6a47d0
XiNiHa <me@xiniha.dev>
2022-05-23 15:02:34 +0900

    Kime 추가

--- components.mjs
+++ components.mjs
@@ -1,4 +1,5 @@
 export default {
   starship: ['./starship.toml', '~/.config', 'starship.toml'],
   zsh: ['./.zshrc-middle.sh', '~/', '.zshrc-middle.sh'],
+  kime: ['./kime.yaml', '~/.config/kime', 'config.yaml'],
 }
--- platforms.mjs
+++ platforms.mjs
@@ -1,4 +1,4 @@
 export default {
   macos: ['zsh', 'starship'],
-  linux: ['zsh', 'starship'],
+  linux: ['zsh', 'starship', 'kime'],
 }
